b81a484d6c8728bdfadb44cd436cb1bd7dfd337b
Luis Angel Ochoa De Los Angeles <luis.odelosangeles@alumnos.udg.mx>
2018-09-22 15:08:05 -0500

    Controlador de usuario ya no está "hardcoreado" Se utilizaron las funciones implementadas en la clase DB en lugar de colocar los datos en el código.

--- controllers/clientCtrl.js
+++ controllers/clientCtrl.js
@@ -1,73 +1,48 @@
-class clientCtrl{
-  constructor(){
-    this.data = [{
-      id :1,
-      name : 'Diego',
-      photo : 'foto.jpeg',
-      sec_name : 'Luis',
-      pat_surname : 'Quiñones',
-      mat_surname : 'Guzmna',
-      company : 'CocaCola',
-      rfc : 'QUDL850321AD2',
-      cfdi : 'archivo.xml',
-      phone : 3387284657,
-      business_name : 'LOS sanos',
-      status : 1,
-      list_email : [
-        'sanoDiego@gmail.com ',
-        'otro@hotmail.com',
-      ],
-      cdu : 'diego123',
-      main_email : 'sanodiego@gmail.com',
-    },
-    {
-      id :2,
-      name : 'Diego',
-      photo : 'foto.jpeg',
-      sec_name : 'Luis',
-      pat_surname : 'Quiñones',
-      mat_surname : 'Guzmna',
-      company : 'CocaCola',
-      rfc : 'QUDL850321AD2',
-      cfdi : 'archivo.xml',
-      phone : 3387284657,
-      business_name : 'LOS sanos',
-      status : 1,
-      list_email : [
-        'sanoDiego@gmail.com ',
-        'otro@hotmail.com',
-      ],
-      cdu : 'diego123',
-      main_email : 'sanodiego@gmail.com',
-    },
-  ];
-  this.getAll = this.getAll.bind(this);
-  this.get = this.get.bind(this);
-  this.create = this.create.bind(this);
-  this.update = this.update.bind(this);
-  this.delete = this.delete.bind(this);
+const db = require('../db');
+const { ClientMdl } = require('../models');
+
+
+class clientCtrl {
+  constructor() {
+    this.getAll = this.getAll.bind(this);
+    this.get = this.get.bind(this);
+    this.create = this.create.bind(this);
+    this.update = this.update.bind(this);
+    this.delete = this.delete.bind(this);
+    this.processResult = this.processResult.bind(this);
   }
 
-  getAll(req, res) {
+  processResult(data) {
+    let result = [];
+    data.forEach((res) => {
+      result.push(new ClientMdl(res));
+    });
+    return result;
+  }
+
+  async getAll(req, res) {
+    this.data = await db.getAll('_Client_');
+    this.data = this.processResult(this.data);
     const json = {
-      response : 'OK',
-      data : this.data
+      response: 'OK',
+      data: this.data,
     };
     res.send(json);
   }
-  get(req, res){
-    const data = this.data.find(el => el.id === Number(req.params.id));
+
+  async get(req, res) {
+    this.data = await db.get('_Client_', req.params.id);
+    this.data = this.processResult(this.data);
     const json = {
-      response : 'OK',
-      data : data
+      response: 'OK',
+      data: this.data,
     };
     res.send(json);
 
   }
-  create(req, res){
-    const lastId = this.data[this.data.length - 1].id;
+
+  create(req, res) {
     const data = {
-      id: lastId + 1,
       name : req.param('name'),
       photo : req.param('photo'),
       sec_name : req.param('sec_name'),
@@ -83,49 +58,50 @@ class clientCtrl{
       cdu : req.param('cdu'),
       main_email : req.param('main_email')
     };
-    this.data.push(data);
+    this.data = db.create('_Client_', data);
     const json = {
       response : 'OK',
-      data : data
+      data : this.data,
     };
     res.status(201).send(json);
 
   }
 
-  update(req, res){
-    let self = this;
-    let id = Number(req.params.id);
-    let data = this.data.find(el => el.id === id);
-    data = {
+  async update(req, res) {
+    const self = this;
+    const data = {
       id : Number(req.param('id')),
-      name : req.param('name') === undefined ? self.data[id-1].name : req.param('name'),
-      photo : req.param('photo') === undefined ? self.data[id-1].photo : req.param('photo'),
-      sec_name : req.param('sec_name') === undefined ? self.data[id-1].sec_name : req.param('sec_name'),
-      pat_surname : req.param('mat_surname') === undefined ? self.data[id-1].pat_surname : req.param('pat_surname'),
-      mat_surname : req.param('mat_surname') === undefined ? self.data[id-1].mat_surname : req.param('mat_surname'),
-      company : req.param('company') === undefined ? self.data[id-1].company : req.param('company'),
-      rfc : req.param('rfc') === undefined ? self.data[id-1].rfc : req.param('rfc'),
-      cfdi : req.param('cfdi') === undefined ? self.data[id-1].cfdi : req.param('cfdi'),
-      phone : req.param('phone') === undefined ? self.data[id-1].phone : req.param('phone'),
-      business_name : req.param('business_name') === undefined ? self.data[id-1].business_name : req.param('business_name'),
-      status : req.param('status') === undefined ? self.data[id-1].status : req.param('status'),
-      list_email : req.param('list_email') === undefined ? self.data[id-1].list_email: req.param('list_email'),
-      cdu : req.param('cdu') === undefined ? self.data[id-1].cdu : req.param('cdu'),
-      main_email : req.param('main_email') === undefined ? self.data[id-1].main_email : req.param('main_email'),
+      name : req.param('name') === undefined ? 'Missing' : req.param('name'),
+      photo : req.param('photo') === undefined ? 'Missing' : req.param('photo'),
+      sec_name : req.param('sec_name') === undefined ? 'Missing' : req.param('sec_name'),
+      pat_surname : req.param('mat_surname') === undefined ? 'Missing' : req.param('pat_surname'),
+      mat_surname : req.param('mat_surname') === undefined ? 'Missing' : req.param('mat_surname'),
+      company : req.param('company') === undefined ? 'Missing' : req.param('company'),
+      rfc : req.param('rfc') === undefined ? 'Missing' : req.param('rfc'),
+      cfdi : req.param('cfdi') === undefined ? 'Missing' : req.param('cfdi'),
+      phone : req.param('phone') === undefined ? 'Missing' : req.param('phone'),
+      business_name : req.param('business_name') === undefined ? 'Missing' : req.param('business_name'),
+      status : req.param('status') === undefined ? false : req.param('status'),
+      list_email : req.param('list_email') === undefined ? 'Missing' : req.param('list_email'),
+      cdu : req.param('cdu') === undefined ? 'Missing' : req.param('cdu'),
+      main_email : req.param('main_email') === undefined ? 'Missing' : req.param('main_email'),
     };
-    this.data[Number(req.params.id) - 1] = data;
+    this.data = await db.update('_Client_', data);
     const json = {
       response : 'OK',
-      data : data
+      data : this.data,
     };
     res.status(201).send(json);
   }
-  delete(req, res){
-    this.data[Number(req.params.id) - 1].status = 0;
-    const data = this.data.find(el => el.id === Number(req.params.id));
+
+  delete(req, res) {
+    const data = {
+      id: req.param('id'),
+    }
+    this.data = db.delete('_Client_', data);
     const json = {
       response : 'OK',
-      data : data
+      data : this.data
     };
     res.status(201).send(json);
   }
