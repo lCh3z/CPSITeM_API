9588acb8238ff4123de0a754169f8540e8165c44
Luis Angel Ochoa De Los Angeles <luis.odelosangeles@alumnos.udg.mx>
2018-09-22 21:53:54 -0500

    Complementada la abstracción de la base de datos  * Se incluyeron los escapes para las entradas en los query.  * Dichas entradas se introducen gracias a los placeholders  * Se agregaron los filtros en los get update  * Se agregaron limites de paginación en los get  * Se agregaron ordenamientos en los get

--- db/index.js
+++ db/index.js
@@ -12,49 +12,89 @@ class DB {
     this.con.connect();
   }
 
-  query() {
-
-  }
-
-  getAll(table) {
-    return new Promise( ( resolve, reject ) => {
-      this.con.query(`SELECT * FROM ${table};`, (err, rows) => {
+  getAll(table, columns, filters, order, limit) {
+    return new Promise((resolve, reject) => {
+      let base = 'SELECT ?? FROM ?? ';
+      let extra = '';
+      const adds = [columns, table];
+      if (filters) {
+        filters.forEach((i, index) => {
+          if (index !== 0) extra += `${i.logic} `;
+          extra += `${this.con.escapeId(i.attr).replace('`', '').replace('`', '')} ${i.oper} `;
+          if (i.oper === 'LIKE') extra += `'%${this.con.escape(i.val).replace('\'', '').replace('\'', '')}%' `; else extra += `${this.con.escape(i.val)} `;
+        });
+      }
+      if (filters) { base += `WHERE ${extra} `; }
+      if (order) {
+        base += `ORDER BY ${this.con.escapeId(order.by)} `;
+        if (order.asc) base += 'ASC '; else base += 'DESC ';
+      }
+      if (limit) base += `Limit ${this.con.escape(limit.start)}, ${limit.quant} `;
+      base += ';';
+      this.con.query(base, adds, (err, rows) => {
         if (err) return reject(err);
         return resolve(rows);
       });
     });
   }
 
-  get(table, id) {
-    return new Promise( ( resolve, reject ) => {
-      this.con.query(`SELECT * FROM ${table} where id=${id};`, ( err, rows ) => {
+  get(table, columns, filters) {
+    return new Promise((resolve, reject) => {
+      let base = 'SELECT ?? FROM ?? ';
+      let extra = '';
+      const adds = [columns, table];
+      filters.forEach((i, index) => {
+        if (index !== 0) extra += `${i.logic} `;
+        extra += `${this.con.escapeId(i.attr).replace('`', '').replace('`', '')} ${i.oper} `;
+        if (i.oper === 'LIKE') extra += `'%${this.con.escape(i.val).replace('\'', '').replace('\'', '')}%' `; else extra += `${this.con.escape(i.val)} `;
+      });
+      base += `WHERE ${extra} ;`;
+      this.con.query(base, adds, (err, rows) => {
         if (err) return reject(err);
         return resolve(rows);
       });
     });
   }
 
-  create(table, object) {
-    return new Promise( ( resolve, reject ) => {
-      this.con.query(`INSERT INTO ${table} (name) values("${object.name}");`, ( err, rows ) => {
+  create(table, post) {
+    return new Promise((resolve, reject) => {
+      this.con.query('INSERT INTO ?? SET ?;', [table, post], (err, rows) => {
         if (err) return reject(err);
         return resolve(rows);
       });
     });
   }
 
-  update(table, object) {
-    return new Promise( ( resolve, reject ) => {
-      this.con.query(`UPDATE ${table} set name = "${object.name}" where id=${object.id};`, ( err, rows ) => {
+  update(table, post, filters) {
+    return new Promise((resolve, reject) => {
+      let base = 'UPDATE ?? SET ? ';
+      let extra = '';
+      console.log(post);
+      const adds = [table, post];
+      filters.forEach((i, index) => {
+        if (index !== 0) extra += `${i.logic} `;
+        extra += `${this.con.escapeId(i.attr).replace('`', '').replace('`', '')} ${i.oper} ${this.con.escape(i.val)} `;
+      });
+      base += `WHERE ${extra} ;`;
+      console.log(base);
+      this.con.query(base, adds, (err, rows) => {
         if (err) return reject(err);
         return resolve(rows);
       });
     });
   }
 
-  delete(table, object) {
-    return new Promise( ( resolve, reject ) => {
-      this.con.query(`DELETE FROM ${table} where id=${object.id};`, ( err, rows ) => {
+  delete(table, filters) {
+    return new Promise((resolve, reject) => {
+      let base = 'DELETE FROM ?? ';
+      let extra = '';
+      filters.forEach((i, index) => {
+        if (index !== 0) extra += `${i.logic} `;
+        extra += `${this.con.escapeId(i.attr).replace('`', '').replace('`', '')} ${i.oper} ${this.con.escape(i.val)} `;
+      });
+      base += `WHERE ${extra} ;`;
+      console.log(base);
+      this.con.query(base, table, (err, rows) => {
         if (err) return reject(err);
         return resolve(rows);
       });
@@ -64,7 +104,6 @@ class DB {
   destructor() {
     this.con.end();
   }
-
 }
 
 module.exports = new DB();
