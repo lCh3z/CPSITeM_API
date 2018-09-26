73bee7cf33767a5b56df36e1be5cbef860ba73c6
Saul Ruiz <pakrisparkour@gmail.com>
2018-09-26 15:45:56 -0500

    Cambiando patch por put en imgStatServR

--- routes/imgStatServR.js
+++ routes/imgStatServR.js
@@ -7,7 +7,7 @@ router.get('/:id_stat_serv', imgStatServCtrl.get);
 
 router.post('/', imgStatServCtrl.create);
 
-router.patch('/:id_stat_serv', imgStatServCtrl.update);
+router.put('/:id_stat_serv', imgStatServCtrl.update);
 
 router.delete('/:id_stat_serv', imgStatServCtrl.delete);
 module.exports = router;
