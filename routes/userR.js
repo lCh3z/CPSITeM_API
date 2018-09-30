a859a94997e471295c4bfa070246239ff4fac7d6
Luis Angel Ochoa De Los Angeles <luis.odelosangeles@alumnos.udg.mx>
2018-09-30 18:03:39 -0500

    Middlewares usados en PUT y POST de User ahora son visualmente más claros

--- routes/userR.js
+++ routes/userR.js
@@ -6,31 +6,39 @@ router.get('/', userCtrl.getAll);
 
 router.get('/:id', userCtrl.get);
 
-router.post('/', (req, res, next) => {
-  middlewares.validator.validate(req, res, next, {
-    body: {
-      main_email: 'email,required',
-    },
-  });
-}, userCtrl.create);
-
-router.put('/:id', (req, res, next) => {
-  middlewares.validator.validate(req, res, next, {
-    body: {
-      name: 'word,required',
-      main_email: 'email,required',
-      list_email: [
-        {
-          email: 'email',
+router.post('/',
+  [
+    (req, res, next) => {
+      middlewares.validator.validate(req, res, next, {
+        body: {
+          main_email: 'email,required',
         },
-      ],
-      worker: {
-        position: 'word',
-        depart: 'word',
-      },
+      });
     },
-  });
-}, userCtrl.update);
+  ],
+  userCtrl.create);
+
+router.put('/:id',
+  [
+    (req, res, next) => {
+      middlewares.validator.validate(req, res, next, {
+        body: {
+          name: 'word,required',
+          main_email: 'email,required',
+          list_email: [
+            {
+              email: 'email',
+            },
+          ],
+          worker: {
+            position: 'word',
+            depart: 'word',
+          },
+        },
+      });
+    },
+  ],
+  userCtrl.update);
 
 router.delete('/:id', userCtrl.delete);
 module.exports = router;
