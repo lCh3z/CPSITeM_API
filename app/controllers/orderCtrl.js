6380d9e4062ada0457baebdbaa0e3d5daf846a97
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-06 17:47:10 -0500

    Modicando controller y model de order

--- app/controllers/orderCtrl.js
+++ app/controllers/orderCtrl.js
@@ -1,5 +1,5 @@
 const db = require('../db');
-const { OrderMdl } = require('../models');
+const { OrderMdl, Responses } = require('../models');
 
 class orderCtrl{
   constructor(){
@@ -8,78 +8,123 @@ class orderCtrl{
     this.create = this.create.bind(this);
     this.update = this.update.bind(this);
     this.delete = this.delete.bind(this);
-    this.processResult = this.processResult.bind(this);
   }
 
-  processResult(data) {
-    const result = [];
-    data.forEach((res) => {
-      result.push(new OrderMdl(res));
-    });
-    return result;
-  }
+  async getAll(req, res, next) {
+    try {
+      const page = parseInt(req.param('page'));
+      const per_page = parseInt(req.param('per_page'));
+      const start = page * per_page;
 
-  async getAll(req, res){
-    let data = await db.getAll('_Order_', ['id', 'id_user', 'id_address', 'id_payment', 'id_cuppon', 'date', 'status'], '', '', '');
-    data = this.processResult(data);
-    if (data.length === 0) {
-      res.status(400).send({ response: 'OK', data: [{ message: 'No existen elementos que cumplan con lo solicitado' }], });
-    } else {
-      res.status(200).send({ data });
+      let data = await OrderMdl.select(
+        '_Order_',
+        [
+          '*',
+        ],
+        null,
+        null,
+        {
+          start,
+          quant: per_page,
+        },
+      );
+
+      if (data.length === 0) {
+        res.status(500).send(Responses.notFound('Order'));
+      } else {
+        const total = await OrderMdl.count(
+          '_Order_',
+          '',
+          '',
+        );
+
+        res.status(200).send({
+          data,
+          per_page,
+          page,
+          total,
+        });
+      }
+    } catch (e) {
+      next(e);
     }
   }
 
-  async get(req, res){
-    let data = await db.get('_Order_', ['id', 'id_user', 'id_address', 'id_payment', 'id_cuppon', 'date', 'status'], [{ attr: 'id', oper: '=', val: Number(req.param('id')) }]);
-    data = this.processResult(data);
-    if (data.length === 0) {
-      res.status(404).send({ error: 'No se encontró el elemento solicitado' });
-    } else {
-      res.status(200).send({ data });
+  async get(req, res, next) {
+    try {
+      let data = await OrderMdl.select(
+        '_Order_',
+        [
+          '*',
+        ],
+        [
+          {
+            attr: 'id',
+            oper: '=',
+            val: Number(req.param('id')),
+          },
+        ],
+        null,
+        null,
+      );
+
+      [data] = data;
+
+      if (!data) {
+        res.status(500).send(Responses.notFound('Order'));
+      }
+      res.status(201).send({ data });
+    } catch (e) {
+      next(e);
     }
   }
 
-  async create(req, res){
-    const newOrder = new OrderMdl(req.body);
-
-    const result = await newOrder.save();
-
-    if(result === 0){
-      res.status(201).send({ message: 'Registrado correctamente' });
-    } else if (result === 1) {
-      res.status(400).send({ error: 'No se pudo registrar' });
+  async create(req, res, next) {
+    try {
+      const Order = new OrderMdl(req.body)
+      const result = await Order.save(req.body);
+      if (result) {
+        return res.status(201).send(Responses.created('Order'));
+      } else {
+        return res.status(500).send(Responses.cantCreate('Order'));
+      }
+    } catch (e) {
+      next(e);
     }
   }
+
   async update(req, res){
-    const Order = new OrderMdl(req.body);
-    Order.id = req.param('id');
+    try {
+      const Order = new OrderMdl(req.body);
+      Order.id =  Number(req.param('id'));
+      const result = await Order.update(req.body);
 
-    const result = await Order.save();
+      if(!result){
+        res.status(500).send(Responses.cantRegister('Order'));
+      }
+      res.status(201).send(Responses.updated('Order'));
+  } catch (e) {
+    next(e);
+  }
+}
 
-    if(result === 0){
-      res.status(200).send({ message: 'Actualizado correctamente' });
-    } else if (result === 1) {
-      res.status(201).send({ message: 'Registrado correctamente'});
-    } else if (result === 2) {
-      res.status(404).send({ error: 'No existe el elemento a actualizar' });
+  async delete(req, res) {
+    try {
+      const Order = new OrderMdl({
+        id: Number(req.param('id')),
+      });
+
+      const result = await Order.delete();
+
+      if(!result){
+        res.status(500).send(Responses.cantDelete('v'));
+      }
+      res.status(201).send(Responses.deleted('Order'));
+    } catch (e) {
+      next(e);
     }
   }
 
-  async delete(req, res){
-    const Order = new OrderMdl({
-      id: Number(req.param('id')),
-    });
-
-    const result = await Order.delete();
-
-    if(result === 0){
-      res.status(200).send({ message: 'Eliminado correctamente' });
-    } else if (result === 1) {
-      res.status(400).send({ error: 'No se pudo eliminar' });
-    } else if (result === 2) {
-      res.status(404).send({ error: 'No existe el elemento a eliminar' });
-    }
-  }
 }
 
 module.exports = new orderCtrl();
--- app/models/orderMdl.js
+++ app/models/orderMdl.js
@@ -1,44 +1,190 @@
 const db = require('../db');
 
 class OrderMdl {
-  constructor(args) {
-    this.id =  args.id;
-    this.id_user  = args.id_user ;
-    this.id_address = args.id_address;
-    this.id_payment = args.id_payment;
-    this.id_cuppon = args.id_cuppon;
-    this.date = args.date;
-    this.status = args.status;
-  }
-
-  processResult(data) {
-    const result = [];
-    data.forEach((res) => {
-      result.push(new OrderMdl(res));
-    });
-    return result;
-  }
-
-  async save() {
-    // Object.keys(this).forEach(key => this[key] === undefined && key !== 'sec_name' && key !== 'photo' && key !== 'company' && delete this[key]);
-    Object.keys(this).forEach(key => this[key] === undefined && delete this[key]);
-    if (this.id !== undefined && this.processResult(await db.get('_Order_', 'id', [{ attr: 'id', oper: '=', val: this.id }])).length !== 0) return this.update();
-    if (await db.create('_Order_', this)) return 0;
-    return 1;
-  }
-
-  async update() {
-    if (this.id !== undefined && await db.update('_Order_', this, [{ attr: 'id', oper: '=', val: this.id }])) return 0;
-    return 1;
-  }
-
-  async delete() {
-    if (this.id !== undefined && this.processResult(await db.get('_Order_', 'id', [{ attr: 'id', oper: '=', val: this.id }])).length !== 0) {
-      if (this.id !== undefined && await db.delete('_Order_', [{ attr: 'id', oper: '=', val: this.id }]) !== undefined) return 0;
-      return 1;
+  constructor(
+    {
+      id,
+      id_user,
+      id_address,
+      id_payment,
+      id_cuppon,
+      status,
+      date,
+      updated,
     }
-    return 2;
+  ) {
+    this.id =  id;
+    this.id_user  = id_user ;
+    this.id_address = id_address;
+    this.id_payment = id_payment;
+    this.id_cuppon = id_cuppon;
+    this.status = status;
+    this.date = date;
+    this.updated = updated;
   }
+
+  static async select(table, columns, filters, order, limit) {
+    try {
+      const data = await db.select(table, columns, filters, order, limit);
+      const response = [];
+      for (const res in data) {
+        const Section = new OrderMdl(data[res]);
+        response.push(Section);
+      }
+      return response;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async count(table, filters) {
+    try {
+      const data = await db.count(table, filters);
+      return data[0].count;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async exists() {
+    try {
+      if (this.id !== undefined) {
+        const result = await db.select(
+          '_Order_',
+          [
+            'id',
+          ],
+          [
+            {
+              attr: 'id',
+              oper: '=',
+              val: this.id,
+            },
+            {
+              logic: 'and',
+              attr: 'status',
+              oper: '!=',
+              val: 0,
+            },
+          ],
+        );
+        return result;
+      }
+      return [];
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async save(list_prod) {
+    const exists = await this.exists();
+    if (this.id !== undefined && exists.length) {
+      return this.update();
+    }
+    try {
+      if (await db.create('_Order_', this)) {
+        const id = await db.select(
+          '_Order_',
+          [
+            'id',
+          ],
+          [
+            {
+              logic: 'and',
+              attr: 'status',
+              oper: '!=',
+              val: 0,
+            },
+          ],
+        );
+        this.id = id[id.length-1].id;
+        console.log('ID: ', this.id);
+        return id[0].id;
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async update(list_prod) {
+    try {
+      const id = await db.select(
+        '_Order_',
+        [
+          'id',
+        ],
+        [
+          {
+            attr: 'id',
+            oper: '=',
+            val: this.id,
+          },
+          {
+            logic: 'and',
+            attr: 'status',
+            oper: '!=',
+            val: 0,
+          },
+        ],
+        null,
+        null,
+      );
+      console.log('id', id[0].id);
+      if (id[0].id === this.id) {
+        delete this.id;
+      }
+      if (this.id !== undefined && await db.update(
+        '_Order_',
+        this,
+        [
+          {
+            attr: 'id',
+            oper: '=',
+            val: this.id,
+          },
+          {
+            logic: 'and',
+            attr: 'status',
+            oper: '!=',
+            val: 0,
+          },
+        ],
+      )) {
+        return this.id;
+      }
+      return false;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+    async delete() {
+      try {
+        const exists = await this.exists();
+        if (exists.length) {
+          if (await db.delete(
+            '_Order_',
+            exists[0],
+            [
+              {
+                attr: 'id',
+                oper: '=',
+                val: this.id,
+              },
+              {
+                logic: 'and',
+                attr: 'status',
+                oper: '!=',
+                val: 0,
+              },
+            ],
+          )) return true;
+        }
+        return false;
+      } catch (e) {
+        throw e;
+      }
+    }
 }
 
 module.exports = OrderMdl;
