a5075ed5288903ac65ea7573595bf9e1dd53a721
Luis Angel Ochoa De Los Angeles <luis.odelosangeles@alumnos.udg.mx>
2018-10-10 15:25:43 -0500

    Fixed error handling in DB/index.js

--- app/db/index.js
+++ app/db/index.js
@@ -181,14 +181,14 @@ class DB {
     const data = this.getDataFromErrorMsg(err.sqlMessage);
     switch (err.code) {
       case 'ER_DUP_ENTRY':
-        error['duplicated'] = {
-          message: `The ${data.field} ${data.data} already exists on the system`,
+        error['Duplicated'] = {
+          message: `${data[1]} already exists on the system`,
           field: data.field,
         };
         break;
       default:
         error['BAD'] = {
-          message: `The ${data.field} ${data.data} already exists on the system`,
+          message: err.sqlMessage,
           field: data.field,
         };
         break;
@@ -198,12 +198,9 @@ class DB {
   }
 
   getDataFromErrorMsg(message) {
-    console.log('msg', message);
-    let data = unescape(message).match(/'([^']+)'/g)
-    return {
-      field: data[1].slice(1,-1),
-      data: data[0].slice(1,-1),
-    }
+    let data = [];
+    data = unescape(message).match(/'([^']+)'/g);
+    return data;
   }
 }
 
