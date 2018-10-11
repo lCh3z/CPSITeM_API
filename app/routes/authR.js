aad721ee1749993bb09267c171759c85427aaa54
Usuario <usuario@usuario.com>
2018-10-11 07:56:05 -0500

    auth ROute

---
+++ app/routes/authR.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { userCtrl } = require('../controllers');
+const middleware = require('../middlewares');
+
+router.post('/register', (req, res)=>{
+  res.send('register');
+});
+router.get('/login', );
+router.get('/logout', );
+
+module.exports = router;
