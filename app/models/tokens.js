21db48146dc2e16c154768ce48daf446bbaafd2e
Usuario <hola@hola.com>
2018-10-11 03:29:39 -0500

    added model tokens

--- app/models/tokens.js
+++ app/models/tokens.js
@@ -1,6 +1,27 @@
 class Token{
-  static add(){
-    //db.insert en la tabla de tokens
-
+  static async add({
+    token,
+    created,
+    id_user,
+    expiter,
+    type,
+    status
+  }){
+    try {
+      await db.create(
+        'tokens',
+        {
+          token,
+          created,
+          id_user,
+          expiter,
+          type,
+          status
+        }
+      );
+    } catch (e) {
+      next(e);
+    }
+    next();
   }
 }
