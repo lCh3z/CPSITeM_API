3ce5045c09ce244a258d9e69f82c76ff7f699a34
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-12 19:48:38 -0500

    addin validator to orderR

--- app/routes/orderR.js
+++ app/routes/orderR.js
@@ -5,9 +5,43 @@ router.get('/', orderCtrl.getAll);
 
 router.get('/:id', orderCtrl.get);
 
-router.post('/', orderCtrl.create);
+router.post('/',
+  [
+    (req, res, next) =>{
+      middlewares.validator.validate(req, res, next,{
+        body:{
+          id_user: 'unsignedInteger, required',
+          id_address: 'unsignedInteger, required',
+          id_payment: 'unsignedInteger, required',
+          id_cuppon: 'unsignedInteger',
+          list_prod:[
+            {
+              id_order: 'unsignedInteger',
+              id_product: 'unsignedInteger',
+              quantity: 'unsignedInteger',
+              price: 'unsignedInteger',
+            }
+          ],
+        },
+      });
+    },
+  ], orderCtrl.create);
 
-router.put('/:id', orderCtrl.update);
+router.put('/:id',
+  [
+    (req, res, next) =>{
+      middlewares.validator.validate(req, res, next,{
+        body:{
+          id: 'unsignedInteger',
+          id_user: 'unsignedInteger, required',
+          id_address: 'unsignedInteger, required',
+          id_payment: 'unsignedInteger, required',
+          id_cuppon: 'unsignedInteger',
+          status: 'unsignedInteger',
+        },
+      });
+    },
+  ], orderCtrl.update);
 
 router.delete('/:id', orderCtrl.delete);
 module.exports = router;
