631b20a1b44fa0887f45ff3fcbad02031661bfca
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-12 19:46:31 -0500

    adding validator to categoryR

--- app/routes/categoryR.js
+++ app/routes/categoryR.js
@@ -5,9 +5,32 @@ router.get('/', categoryCtrl.getAll);
 
 router.get('/:id', categoryCtrl.get);
 
-router.post('/', categoryCtrl.create);
+router.post('/',
+  [
+    (req, res, next) =>{
+      middlewares.validator.validate(req, res, next,{
+        body:{
+          name: 'word, required',
+          description: 'string, required',
+          photo: 'photo',
+        },
+      });
+    },
+  ], categoryCtrl.create);
 
-router.put('/:id', categoryCtrl.update);
+router.put('/:id',
+  [
+    (req, res, next) =>{
+      middlewares.validator.validate(req, res, next,{
+        body:{
+          name: 'word, required',
+          description: 'string, required',
+          photo: 'photo',
+          status: 'unsignedInteger',
+        },
+      });
+    },
+  ], categoryCtrl.update);
 
 router.delete('/:id', categoryCtrl.delete);
 module.exports = router;
