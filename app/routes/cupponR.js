0a605ab6ed3a81210d1e65e71c80a8f9f436b092
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-12 19:47:32 -0500

    addin validator to cupponR

--- app/routes/cupponR.js
+++ app/routes/cupponR.js
@@ -1,3 +1,4 @@
+
 const router = require('express').Router();
 const { cupponCtrl } = require('../controllers');
 
@@ -5,9 +6,32 @@ router.get('/', cupponCtrl.getAll);
 
 router.get('/:id', cupponCtrl.get);
 
-router.post('/', cupponCtrl.create);
+router.post('/',
+  [
+    (req, res, next) =>{
+      middlewares.validator.validate(req, res, next,{
+        body:{
+          code: 'word, required',
+          discount: 'unsignedInteger, required',
+          description: 'word, required',
+        },
+      });
+    },
+  ], cupponCtrl.create);
 
-router.put('/:id', cupponCtrl.update);
+router.put('/:id',
+  [
+    (req, res, next) =>{
+      middlewares.validator.validate(req, res, next,{
+        body:{
+          code : 'word, required',
+          discount: 'unsignedInteger, required',
+          description: 'word, required',
+          status: 'unsignedInteger, required',
+        },
+      });
+    },
+  ], cupponCtrl.update);
 
 router.delete('/:id', cupponCtrl.delete);
 module.exports = router;
