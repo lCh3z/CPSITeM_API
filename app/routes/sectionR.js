9b2358981e8283900593d330a9376446c3bb80ab
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-12 19:49:39 -0500

    addin validator to sectionR

--- app/routes/sectionR.js
+++ app/routes/sectionR.js
@@ -5,9 +5,43 @@ router.get('/', sectionCtrl.getAll);
 
 router.get('/:id', sectionCtrl.get);
 
-router.post('/', sectionCtrl.create);
+router.post('/',
+  [
+    (req, res, next) =>{
+      middlewares.validator.validate(req, res, next,{
+        body:{
+          type: 'unsignedInteger',
+          status: 'unsignedInteger',
+          conf_section:[
+            {
+              title: 'string, required',
+              subtitle: 'string',
+              description: 'string',
+            }
+          ],
+        },
+      });
+    },
+  ], sectionCtrl.create);
 
-router.put('/:id', sectionCtrl.update);
+router.put('/:id',
+  [
+    (req, res, next) =>{
+      middlewares.validator.validate(req, res, next,{
+        body:{
+          type: 'unsignedInteger',
+          status: 'unsignedInteger',
+          conf_section:[
+            {
+              title: 'string, required',
+              subtitle: 'string',
+              description: 'string',
+            }
+          ],
+        },
+      });
+    },
+  ], sectionCtrl.update);
 
 router.delete('/:id', sectionCtrl.delete);
 module.exports = router;
