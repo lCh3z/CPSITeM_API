035f356ea153dc2650ade20c59648b249ac1d972
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-12 19:56:03 -0500

    addin validator to wishlistR

--- app/routes/wishListR.js
+++ app/routes/wishListR.js
@@ -3,9 +3,29 @@ const { wishListCtrl } = require('../controllers');
 
 router.get('/', wishListCtrl.getAll);
 
-router.post('/', wishListCtrl.create);
+router.post('/',
+  [
+    (req, res, next) =>{
+      middlewares.validator.validate(req, res, next,{
+        body:{
+          id_user: 'unsignedInteger, required',
+          id_product: 'unsignedInteger, required',
+        },
+      });
+    },
+  ], wishListCtrl.create);
 
-router.put('/:id_user', wishListCtrl.update);
+router.put('/:id_user',
+  [
+    (req, res, next) =>{
+      middlewares.validator.validate(req, res, next,{
+        body:{
+          id_product: 'unsignedInteger, required',
+          status: 'unsignedInteger',
+        },
+      });
+    },
+  ], wishListCtrl.update);
 
 router.delete('/:id_user', wishListCtrl.delete);
 
