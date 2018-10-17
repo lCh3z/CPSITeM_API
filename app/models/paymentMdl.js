b81dc1d40ee744b265e6ae01941c0eb27316f05d
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-16 20:21:49 -0500

    adding documentation to paymentMdl

--- app/models/paymentMdl.js
+++ app/models/paymentMdl.js
@@ -26,6 +26,17 @@ class PaymentMdl {
     this.updated = updated;
   }
 
+  /**
+   * @async
+   * Async function that from the table _Payment_ select all the posible tuples
+   * with the designated params and returns a promise
+   * @param  {string}  table   Table required (_Payment_) of the database
+   * @param  {Array.<string>}  columns Required columns of de table _Payment_ from the database
+   * @param  {Array.<object>}  filters list of filter objects to use.
+   * @param  {Object}  order   Nullable definition of ORDER paramns.
+   * @param  {Object}  limit   Nullable definition of LIMIT params.
+   * @return {Promise}         Return a promise with the information from the database.
+   */
   static async select(table, columns, filters, order, limit) {
     try {
       const data = await db.select(table, columns, filters, order, limit);
