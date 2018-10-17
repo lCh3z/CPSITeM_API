0c709a4a6f602b80bd1f1827db5293ea4d89ac39
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-16 19:10:02 -0500

    adding documentatidon to cartMdl

--- app/models/cartMdl.js
+++ app/models/cartMdl.js
@@ -1,5 +1,8 @@
 const db = require('../db');
-
+/**
+ * @classdesc Class model of cart.contains methods such as select, save, exists
+ * save, update, delete and processResult
+ */
 class CartMdl {
   constructor(
     {
@@ -27,6 +30,17 @@ class CartMdl {
     return result;
   }
 
+  /**
+   * <qasync
+   *Async function that from the table _Cart_ select all the posible tuples
+   *with the designated params and returns a promise
+   * @param  {string}  table   Table required (_Cart_) of the database
+   * @param  {Array.<string>}  columns Required columns of de table _Cart_ from the database
+   * @param  {Array.<object>}  filters list of filter objects to use.
+   * @param  {Object}  order   Nullable definition of ORDER paramns.
+   * @param  {Object}  limit   Nullable definition of LIMIT params.
+   * @return {Promise}         Return a promise with the information from the database.
+   */
   static async select(table, columns, filters, order, limit) {
     try {
       const data = await db.select(table, columns, filters, order, limit);
@@ -40,6 +54,12 @@ class CartMdl {
     }
   }
 
+  /**
+   * @async
+   * Async funciton that checks if a cart already exists in the
+   *  table _Cart_ of the Database
+   * @return {Promise} Return a promise with the information from the database.
+   */
   async exists() {
     try {
       if (this.id_user !== undefined && this.id_product !== undefined) {
@@ -76,6 +96,16 @@ class CartMdl {
     }
   }
 
+  /**
+   * @async
+   *Async funcitonthat checks if a cart already exists, it will be updated, if not
+   * it will be created in the table _Cart_ in the database
+   *
+   * @return {Promise} Returns a promise,
+   *                    - updated if it already exists
+   *                    - true if it is created a new one
+   *                    - false if it could not be created
+   */
   async save() {
     try {
       const exists = await this.exists();
@@ -91,6 +121,13 @@ class CartMdl {
     }
   }
 
+  /**
+   * @async
+   * Async funciton that updates a cart from the table _Cart_ in the Database
+   * @return {Promise} Returns a Promise
+   *                   - Returns true if it could be updated
+   *                   - Returns false if it could not be updated
+   */
   async update() {
     try {
       if (this.id_user !== undefined && this.id_product !== undefined && await db.update(
@@ -122,6 +159,14 @@ class CartMdl {
     }
   }
 
+  /**
+   * @async
+   * Async function that deletes a cart from the table _Cart_ in the database .
+   * It will check first if the tuple to delete exists
+   *
+   * @return {Promise} Returns a Promise
+   *                   - Return true if it could be deleted
+   */
   async delete() {
     const exists = await this.exists();
     if (exists.length) {
