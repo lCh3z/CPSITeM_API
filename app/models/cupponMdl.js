ab2e241922e417a90fbbdeb1c69e7dd77dff7d59
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-16 19:56:40 -0500

    adding documentation to cupponnMdl

--- app/models/cupponMdl.js
+++ app/models/cupponMdl.js
@@ -110,6 +110,17 @@ class CupponMdl {
     }
   }
 
+  /**
+   * @async
+   * Async funciton that checks if a cuppon already exists, it will be updated, if not
+   * it will be created in the table _Cuppon_ in the database
+   *
+   * @return {Promise} Returns a promise,
+   *                    - updated if it already exists
+   *                    - true if it is created a new one
+   *                    - false if it could not be created
+   * @version 15/10/2018
+   */
   async save() {
     try {
       const exists = await this.exists();
