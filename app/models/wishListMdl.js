f6ff09357b4695f096a6bae252dffcbd4dc492af
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-16 20:34:17 -0500

    adding documentation to wishlistMdl

--- app/models/wishListMdl.js
+++ app/models/wishListMdl.js
@@ -133,6 +133,15 @@ class WishlistMdl {
     }
   }
 
+  /**
+   * @async
+   * Async function that deletes a wishlist from the table _WishList_ in the database .
+   * It will check first if the tuple to delete exists
+   *
+   * @return {Promise} Returns a Promise
+   *                   - Return true if it could be deleted
+   * @version 15/10/2018
+   */
   async delete() {
     const exists = await this.exists();
     if (exists.length) {
