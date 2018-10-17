5016720fc90aeb5190ac60d7331d86e9743e6e91
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-16 19:57:13 -0500

    adding documentation to cupponnMdl

--- app/models/cupponMdl.js
+++ app/models/cupponMdl.js
@@ -175,6 +175,15 @@ class CupponMdl {
     }
   }
 
+  /**
+   * @async
+   * Async function that deletes a cuppon from the table _Cuppon_ in the database .
+   * It will check first if the tuple to delete exists
+   *
+   * @return {Promise} Returns a Promise
+   *                   - Return true if it could be deleted
+   * @version 15/10/2018
+   */
   async delete() {
     try {
       const exists = await this.exists();
