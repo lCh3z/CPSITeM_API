7b2c423c930a0b8d9553f03564f37d7e0e4bcb24
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-16 19:43:04 -0500

    adding documentation to categoryMdl

--- app/models/categoryMdl.js
+++ app/models/categoryMdl.js
@@ -159,6 +159,15 @@ class CategoryMdl{
     }
   }
 
+  /**
+   * @async
+   * Async function that deletes a cart from the table _Category_ in the database .
+   * It will check first if the tuple to delete exists
+   *
+   * @return {Promise} Returns a Promise
+   *                   - Return true if it could be deleted
+   * @version 15/10/2018
+   */
   async delete() {
     try {
       const exists = await this.exists();
