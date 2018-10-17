4d1ad88061004e3994a4e350c28ef1375fe49d93
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-16 20:24:03 -0500

    adding documentation to productMdl

--- app/models/categoryMdl.js
+++ app/models/categoryMdl.js
@@ -72,7 +72,7 @@ class CategoryMdl{
   /**
    * @async
    * Async funciton that checks if a category already exists in the
-   *  table _Category_ of the Database
+   * table _Category_ of the Database
    * @return {Promise} Return a promise with the information from the database.
    * @version 15/10/2018
    */
@@ -108,7 +108,7 @@ class CategoryMdl{
 
   /**
    * @async
-   *Async funcitonthat checks if a category already exists, it will be updated, if not
+   * Async funcitonthat checks if a category already exists, it will be updated, if not
    * it will be created in the table _Category_ in the database
    *
    * @return {Promise} Returns a promise,
