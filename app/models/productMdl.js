52de549e1a77a7e39f561c89b3f0c77d25a302c0
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-16 20:26:36 -0500

    adding documentation to productMdl

--- app/models/productMdl.js
+++ app/models/productMdl.js
@@ -120,6 +120,17 @@ class ProductMdl {
     }
   }
 
+  /**
+   * @async
+   * Async funcitonthat checks if a product already exists, it will be updated, if not
+   * it will be created in the table _Product_ in the database
+   *
+   * @return {Promise} Returns a promise,
+   *                    - updated if it already exists
+   *                    - true if it is created a new one
+   *                    - false if it could not be created
+   * @version 15/10/2018
+   */
   async save(list_imgs) {
     try {
       const exists = await this.exists();
