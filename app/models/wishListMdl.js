4ea568eab00a10acff576b1b0d659e71fefec2e0
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-16 20:32:01 -0500

    adding documentation to wishlistMdl

--- app/models/wishListMdl.js
+++ app/models/wishListMdl.js
@@ -1,5 +1,9 @@
 const db = require('../db');
-
+/**
+ * @classdesc Class model of wishlist.contains methods such as select, save, exists
+ * save, update, delete and processResult
+ * @version 15/10/2018
+ */
 class WishlistMdl {
   constructor(
       {
