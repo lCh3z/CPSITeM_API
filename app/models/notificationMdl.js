e9e9c819cca7d7c52bf242a62096684daa553dec
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-16 20:03:10 -0500

    adding documentation to notificationMdl

--- app/models/notificationMdl.js
+++ app/models/notificationMdl.js
@@ -50,6 +50,18 @@ class NotificationMdl {
     }
   }
 
+  /**
+   * @async
+   * Async function that reciebes two parameters.
+   * The first one is the table (_Notification_) to look for in the Database
+   * The second parameter are the filters to aply to the search
+   * It will return a promise with the total count
+   * @param  {string}  table   Table to look for in the database
+   * @param  {Array.<object>}  filters filters to be applied to the search
+   * @return {Promise}         Returns a promise with the total count of tuples
+   *                           found
+   * @version 15/10/2018
+   */
   static async count(table, filters) {
     try {
       const data = await db.count(table, filters);
