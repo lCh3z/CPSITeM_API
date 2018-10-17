0d8297802b57eb5bcbc98d361edeecf93395e512
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-16 19:10:41 -0500

    adding documentatidon to cartMdl

--- app/models/cartMdl.js
+++ app/models/cartMdl.js
@@ -129,7 +129,14 @@ class CartMdl {
     }
   }
 
-
+  /**
+   * @async
+   * Async function that deletes a cart from the table _Cart_ in the database .
+   * It will check first if the tuple to delete exists
+   *
+   * @return {Promise} Returns a Promise
+   *                   - Return true if it could be deleted
+   */
   async delete() {
     const exists = await this.exists();
     if (exists.length) {
