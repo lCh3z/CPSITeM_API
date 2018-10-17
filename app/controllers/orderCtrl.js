c227700869b428c739c9e718bfe9077c60f7ffe0
Ulises Ruelas <u.m.r.b._@hotmail.com>
2018-10-16 19:30:53 -0500

    Controlles documentados

--- app/controllers/orderCtrl.js
+++ app/controllers/orderCtrl.js
@@ -1,6 +1,13 @@
 const db = require('../db');
 const { OrderMdl, Responses } = require('../models');
 
+/**
+ *
+ * @classdesc Class of controller Order, contain the getAll, get, create, update and delete
+ *             alike a functions, all are initialize with the information
+ *            of his ".bind"
+ * @version   15/10/2018
+ */
 class orderCtrl {
   constructor() {
     this.getAll = this.getAll.bind(this);
@@ -10,6 +17,18 @@ class orderCtrl {
     this.delete = this.delete.bind(this);
   }
 
+  /**
+   * @async
+   * Async function to get all the data from the model of OrderMdl (DB), depending
+   * the recived response with a error of notFound or send the got data,can catch a error
+   * and calls the next with the error
+   * @param  {Request Object}     req   Request to the function, includes information in params
+   * @param  {Response Object}    res   Response than will give the function
+   * @param  {Next Object}        next  In case of be necessary go by a other the work or
+   *                                    if spawn a error
+   * @return {Promise}                  Promise to return the data results
+   * @version 15/10/2018
+   */
   async getAll(req, res, next) {
     try {
       let page = parseInt(req.param('page'));
@@ -84,6 +103,18 @@ class orderCtrl {
     }
   }
 
+  /**
+   * @async
+   * Async function to get a specific Order used model of Order with a select to the
+   * database, depending the recived from the model response with a error or send the got
+   * data, can catch a error and calls the next with error
+   * @param  {Request Object}     req   Request to the function, includes information in params
+   * @param  {Response Object}    res   Response than will give the function
+   * @param  {Next Object}        next  In case of be necessary go by a other the work or
+   *                                    if spawn a error
+   * @return {Promise}                  Promise to return the data results
+   * @version 15/10/2018
+   */
   async get(req, res, next) {
     try {
       let data = await OrderMdl.select(
@@ -113,6 +144,19 @@ class orderCtrl {
     }
   }
 
+  /**
+   * @async
+   * Async function to create a new Order, the controller response depending if
+   * a promise of Order.save() responses sending a especific response of created a
+   * Order, can catch a error and calls the next whit the error
+   * @param  {Request Object}     req   Request to the function, includes information in params
+   * @param  {Response Object}    res   Response than will give the function
+   * @param  {Next Object}        next  In case of be necessary go by a other the work or
+   *                                    if spawn a error
+   * @return {Promise, Response}        Promise return a response of created or can´t be created
+   *
+   * @version 15/10/2018
+   */
   async create(req, res, next) {
     try {
       const Order = new OrderMdl(req.body);
@@ -128,6 +172,20 @@ class orderCtrl {
     }
   }
 
+  /**
+   * @async
+   * Async function to update data from the model of Order, the controller update
+   * the data from OrderMdl with the request information, depending a result of save
+   * data it indicates if the data was updated of not, can catch a error and calls
+   * the next with the error
+   * @param  {Request Object}     req   Request to the function, includes information in params
+   * @param  {Response Object}    res   Response than will give the function
+   * @param  {Next Object}        next  In case of be necessary go by a other the work or
+   *                                    if spawn a error
+   * @return {Promise, Response}        Promise return a response of updated or can´t be registered
+   *
+   * @version 15/10/2018
+   */
   async update(req, res, next) {
     try {
       const Order = new OrderMdl(req.body);
@@ -143,6 +201,21 @@ class orderCtrl {
       next(e);
     }
   }
+
+  /**
+   * @async
+   * Async function to delete data from the model of Order, the controller delete data from
+   * the model of Order with the request information, next to it indicates to Order the delete that data,
+   * depending the result if can be deleted response if data was or not deleted, can catch a error
+   * and calls next with error
+   * @param  {Request Object}     req   Request to the function, includes information in params
+   * @param  {Response Object}    res   Response than will give the function
+   * @param  {Next Object}        next  In case of be necessary go by a other the work or
+   *                                    if spawn a error
+   * @return {Promise, Response}        Promise return a response of can´t be deleted or deleted
+   *
+   * @version 15/10/2018
+   */
   async delete(req, res, next) {
     try {
       const Order = new OrderMdl({
