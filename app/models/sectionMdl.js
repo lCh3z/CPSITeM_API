1843e5908e539510da7a6596a8cf42818e7d8f56
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-16 20:44:23 -0500

    adding documentation to sectionMdl

--- app/models/sectionMdl.js
+++ app/models/sectionMdl.js
@@ -107,6 +107,17 @@ class SectionMdl {
     }
   }
 
+  /**
+   * @async
+   * Async funcitonthat checks if a section already exists, it will be updated, if not
+   * it will be created in the table _Section_ in the database
+   *
+   * @return {Promise} Returns a promise,
+   *                    - updated if it already exists
+   *                    - true if it is created a new one
+   *                    - false if it could not be created
+   * @version 15/10/2018
+   */
   async save(conf_section) {
     const exists = await this.exists();
     if (this.id !== undefined && exists.length) {
