24fbef41b366399eb4ab83ec110e2490a576738d
Saul Ruiz <pakrisparkour@gmail.com>
2018-10-16 20:33:06 -0500

    adding documentation to wishlistMdl

--- app/models/wishListMdl.js
+++ app/models/wishListMdl.js
@@ -39,6 +39,18 @@ class WishlistMdl {
     return result;
   }
 
+  /**
+   * @async
+   * Async function that from the table _WishList_ select all the posible tuples
+   * with the designated params and returns a promise
+   * @param  {string}  table   Table required (_WishList_) of the database
+   * @param  {Array.<string>}  columns Required columns of de table _WishList_
+   *                                   from the database
+   * @param  {Array.<object>}  filters list of filter objects to use.
+   * @param  {Object}  order   Nullable definition of ORDER paramns.
+   * @param  {Object}  limit   Nullable definition of LIMIT params.
+   * @return {Promise}         Return a promise with the information from the database.
+   */
   static async select(table, columns, filters, order, limit) {
     try {
       const data = await db.select(table, columns, filters, order, limit);
