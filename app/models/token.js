52b8357aa291fe50689875e60b39f6f09f368898
Luis Angel Ochoa De Los Angeles <luis.odelosangeles@alumnos.udg.mx>
2018-10-19 10:42:08 -0500

    Improved token model

--- app/models/token.js
+++ app/models/token.js
@@ -17,32 +17,79 @@ class Token {
     this.token = token;
     this.id_user = id_user;
     this.type = type;
-    this.expires = expires;
+    this.expires = null;
     this.status = status;
     this.date = date;
     this.updated = updated;
+
+    this.updateExpire();
   }
 
-  static add({
-    token,
-    id_user,
-    type,
-    expires,
-    status
-  }) {
+  updateExpire() {
+    if (this.type) {
+      this.expires = Date.now() + Number(process.env[this.type]) * 60000;
+      this.expires = new Date(this.expires).toISOString().slice(0, 19).replace('T', ' ');
+    }
+    return null;
+  }
+
+  async exists() {
     try {
-      return db.create(
-        '_Token_',
-        {
-          token,
-          id_user,
-          type,
-          expires,
-          status
-        }
-      );
+      if (this.token !== undefined) {
+        return await db.select(
+          '_Token_',
+          [
+            '*',
+          ],
+          [
+            {
+              attr: 'token',
+              oper: '=',
+              val: this.token,
+            },
+            {
+              logic: 'and',
+              attr: 'status',
+              oper: '!=',
+              val: 0,
+            },
+          ],
+        );
+      }
+      return [];
     } catch (e) {
-      throw (e);
+      throw e;
+    }
+  }
+
+  async save() {
+    const tempToken = await this.exists();
+    if (tempToken.length && this.id !== undefined) {
+      try {
+        tempToken.expires = this.expires;
+        return db.update(
+          '_Token_',
+          this,
+          [
+            {
+              attr: 'id',
+              oper: '=',
+              val: this.id,
+            },
+          ],
+        );
+      } catch (e) {
+        throw e;
+      }
+    } else {
+      try {
+        return db.create(
+          '_Token_',
+          this,
+        );
+      } catch (e) {
+        throw e;
+      }
     }
   }
 }
--- app/routes/authR.js
+++ app/routes/authR.js
@@ -7,7 +7,6 @@ router.post('/register', (req, res, next) => {
 }, (req, res) => {
   res.header('Authorization', `Bearer ${req.body.message.token}`);
   res.status(200).send({ message: req.body.message });
-  console.log('RES', res);
 });
 
 // router.post('/login', );
