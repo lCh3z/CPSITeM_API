b4d9854a62a9b78c1c9d96889d5445a24c883d3f
Luis Angel Ochoa De Los Angeles <luis.odelosangeles@alumnos.udg.mx>
2018-10-24 20:29:39 -0500

    added unique to address name

--- docs/docs/db.sql
+++ docs/docs/db.sql
@@ -179,7 +179,7 @@ CREATE TABLE _Cuppon_(
 create table _Address_(
   id integer unsigned not null,
   id_user integer unsigned not null,
-  name varchar (48),
+  name varchar (48) unique,
   reference varchar (128),
   street varchar (48),
   colony varchar (48),
